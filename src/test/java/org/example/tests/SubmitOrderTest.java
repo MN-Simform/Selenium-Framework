2dd04b493883a348b9449a31ec05dde9eb06a7e2
MN-Simform <nimit@simformsolutions.com>
2024-09-25 15:17:43 +0530

    Removed Unwanted Packages

--- src/test/java/org/example/tests/SubmitOrderTest.java
+++ src/test/java/org/example/tests/SubmitOrderTest.java
@@ -7,16 +7,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import org.example.TestComponents.BaseTest;
